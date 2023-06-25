383cb21851a1542ae48dbedf8c730cf7ecfc6747
HappyFight0 <ayushkanaujiya97@gmail.com>
2023-06-25 14:47:41 +0545

    Added summary module inside main.py

--- Client-Side/Amazon.py
+++ Client-Side/Amazon.py
@@ -91,7 +91,7 @@ def web_scraping_amazon(product_link):
             product_info = scrape_product_details_amazon(final_product_link)
             related_products.append(product_info)
 
-        if len(related_products) == 5:
+        if len(related_products) == 1:
             return related_products
     return related_products
 
--- Client-Side/main.py
+++ Client-Side/main.py
@@ -1,6 +1,45 @@
 from Amazon import web_scraping_amazon
 from Flipkart import web_scraping_flipkart
 
+import openai
+import os
+
+from dotenv import load_dotenv, find_dotenv
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0.25,
+    )
+    return response.choices[0].message["content"]
+
+
+def Summarize(content):
+    _ = load_dotenv(find_dotenv())
+    openai.api_key = os.getenv('OPENAI_API_KEY')
+
+    prod_review = content
+
+    prompt = f"""
+    Your task is to generate a short summary of a product \
+    review from an ecommerce site to give overall idea \
+    about the product to the new customer. 
+
+    Summarize the review below, delimited by triple 
+    backticks, in at most 150 words, and focusing on quality of\
+    the product, the good and bad in terms of price and satisfaction \
+    level. Do consider if the customer had any issue in the delivery \
+    and customer service. Finally also state the bads of the product \
+    as well. Also include line breaks where necessay in the response.
+
+    Review: ```{prod_review}```
+    """
+
+    response = get_completion(prompt)
+    return response
+
 
 def search_product(key):
     amazon_link = 'https://www.amazon.in/s?k=' + key
